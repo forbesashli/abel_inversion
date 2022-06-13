3185aa0a947926849420f4bd25c7cf6e74fb8880
Ashli Forbes <44144720+forbesashli@users.noreply.github.com>
2022-06-13 10:39:09 -0700

    integral_pushes

--- abel_inversion.py
+++ abel_inversion.py
@@ -1,3 +1,25 @@
+from pickle import TRUE
 import numpy as np
+import scipy as sp
+import sympy as smp
 
 # test test
+
+
+class cv:  # cord_variables
+    A_VALUE = 3
+    X_VALUE = 1
+    Y_VALUE = 1
+
+
+class abel_inversion:
+    def abel_inversion():
+        x = smp.symbols("x", real=TRUE)
+        r = (np.power(cv.A_VALUE, 2) - np.power(cv.Y_VALUE, 2)) ** (1 / 2)
+        f = smp.sin(x) ** 3 * smp.exp(-5 * x)
+        result = smp.integrate(f, x)
+
+        print(r, result)
+
+
+abel_inversion.abel_inversion()
