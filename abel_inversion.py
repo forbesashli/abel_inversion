998ce7b9eaaeef3ae4022d637ba20daf8d2f17bf
Ashli Forbes <44144720+forbesashli@users.noreply.github.com>
2022-06-13 14:02:19 -0700

    integral_addtion

--- abel_inversion.py
+++ abel_inversion.py
@@ -1,25 +1,43 @@
-from pickle import TRUE
+from cgitb import reset
+from unittest import result
 import numpy as np
 import scipy as sp
 import sympy as smp
+import scipy.integrate as integrate
+import scipy.special as special
 
 # test test
 
 
 class cv:  # cord_variables
-    A_VALUE = 3
-    X_VALUE = 1
-    Y_VALUE = 1
+    A_VALUE = 2
+    X_VALUE = 1.414
+    Y_VALUE = 1.414
 
 
-class abel_inversion:
-    def abel_inversion():
-        x = smp.symbols("x", real=TRUE)
+class A_I:
+    def integrand(y, f_r):
+        r = smp.symbols("r", real=True)
+
+        return (
+            2 * r * f_r / (smp.sqrt(r ** smp.Rational(2, 1) - y ** smp.Rational(2, 1)))
+        )
+
+    def abel_inversion(a, y, f_r):
+        return smp.integrate(A_I.integrand(y, f_r), (r, y, a)).evalf()
+
+    def test_integrals():
+
+        x = smp.symbols("x", real=True)
         r = (np.power(cv.A_VALUE, 2) - np.power(cv.Y_VALUE, 2)) ** (1 / 2)
         f = smp.sin(x) ** 3 * smp.exp(-5 * x)
-        result = smp.integrate(f, x)
+        result = integrate.quad(lambda x: special.jv(2.5, x), 0, 4.5)
+        print(result)
 
-        print(r, result)
 
-
-abel_inversion.abel_inversion()
+r = smp.symbols("r", real=True)
+f_r = r + 9
+a = 2
+y = 3
+print(A_I.abel_inversion(a, y, f_r))
+# A_I.test_integrals()
